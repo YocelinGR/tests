00f558c27e6008dc822c6643232490202a660622
Yocelin Garcia Romero <garcia_romero.y@hotmail.com>
2019-01-07 16:09:01 -0600

    first commit

---
+++ src/components/CompaniesComponent.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="column companies is-half">
+    <EnterpriseBanner></EnterpriseBanner>
+    <ItemCompany></ItemCompany>
+    <ItemCompany></ItemCompany>
+    <ItemCompany></ItemCompany>
+  </div>
+</template>
+
+<script>
+import EnterpriseBanner from "./EnterpriseBanner.vue";
+import ItemCompany from "./ItemCompany.vue";
+
+export default {
+  props: ["company"],
+  data: function() {
+    return {
+      companysData: this.company
+    };
+  },
+  components: {
+    EnterpriseBanner: EnterpriseBanner,
+    ItemCompany: ItemCompany
+  },
+  mounted() {
+    this.companysData = JSON.parse(localStorage.getItem("companies"));
+  }
+};
+</script>
+
